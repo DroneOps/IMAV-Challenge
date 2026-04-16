e27cf9ca09353fb8ec2eb4aa7cd289b0e98712f2
jsencinas <jesanchezencinas@gmail.com>
2026-04-16 17:23:59 -0600

    Fixed logger matrix creation

--- src/logger.py
+++ src/logger.py
@@ -2,22 +2,17 @@ import csv
 
 class Logger:
     fields = ["x_velocity", "y_velocity", "z_velocity"]
-    rows = [[], [], []]
+    rows = []
 
     def __init__(self):
         pass
     
-    def update_info(self, x_velocity:int, y_velocity:int, z_velocity:int):
-        self.rows[0].append(x_velocity)
-        self.rows[1].append(y_velocity)
-        self.rows[2].append(z_velocity)
-        self.save_to_file()
-
     def save_to_file(self):
         with open('log.csv', 'w') as csvfile:
             csvwriter = csv.writer(csvfile) # Create writer object
             csvwriter.writerow(self.fields) # Write header
             csvwriter.writerows(self.rows) # Write multiple rows
-
-    def __del__(self):
+    
+    def update_info(self, x_velocity:int, y_velocity:int, z_velocity:int):
+        self.rows.append([x_velocity, y_velocity, z_velocity])
         self.save_to_file()
