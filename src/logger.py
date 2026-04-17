d6daab849eee2da4e354efacba67691946e07dc5
jsencinas <jesanchezencinas@gmail.com>
2026-04-17 13:46:49 -0600

    csv data allocation works, plot still pending

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,9 @@
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class Logger:
-    fields = ["x_velocity", "y_velocity", "z_velocity"]
-    rows = []
+    matrix = [["x_velocity", "y_velocity", "z_velocity"]]
 
     def __init__(self):
         pass
@@ -10,9 +11,13 @@ class Logger:
     def save_to_file(self):
         with open('log.csv', 'w') as csvfile:
             csvwriter = csv.writer(csvfile) # Create writer object
-            csvwriter.writerow(self.fields) # Write header
-            csvwriter.writerows(self.rows) # Write multiple rows
+            csvwriter.writerows(self.matrix) # Write multiple rows
     
     def update_info(self, x_velocity:int, y_velocity:int, z_velocity:int):
-        self.rows.append([x_velocity, y_velocity, z_velocity])
+        self.matrix.append([x_velocity, y_velocity, z_velocity])
         self.save_to_file()
+    
+    def plot_log(self):
+        df = pd.DataFrame(self.matrix[1:], columns=self.matrix[0])
+        plt.plot(df["x_velocity"])
+        plt.show()
